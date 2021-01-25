fe68ac98df8d0a44a7209dc8d32e8c9c58c733b5
Takafumi Arakaki <aka.tkf@gmail.com>
2021-01-24 17:24:34 -0800

    Extend DATA_LENGTH of examples/findfirst/

--- examples/findfirst/benchmarks.jl
+++ examples/findfirst/benchmarks.jl
@@ -3,12 +3,12 @@ using Folds
 using FoldsThreadsExtras
 
 const SUITE = BenchmarkGroup()
-const DATA_LENGTH = 2^20
+const DATA_LENGTH = 2^25
 
 for log2_basesize in 10:2:15
     basesize = 2^log2_basesize
     SUITE[basesize] = s1 = BenchmarkGroup()
-    for needleloc in floor.(Int, range(2, 2^20, length = 10))
+    for needleloc in floor.(Int, range(2, DATA_LENGTH, length = 10))
         s1[needleloc] = s2 = BenchmarkGroup()
         xs = rand(DATA_LENGTH)
         xs[needleloc] = 2
