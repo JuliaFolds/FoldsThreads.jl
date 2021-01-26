c51327fb643d7be88c646581293a98cbaf579827
Takafumi Arakaki <aka.tkf@gmail.com>
2021-01-25 19:38:20 -0800

    Set up deploydocs (#2)

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,4 @@ makedocs(
     modules = [FoldsThreads]
 )
 
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
+deploydocs(; repo = "github.com/JuliaFolds/FoldsThreads.jl", push_preview = true)
